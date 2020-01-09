38b651d2f6844a0967eab4e1ebf5bb5943143f5a
SAISURAJ <saisuraj@saisurajs-mac.local>
2020-01-09 11:55:08 +0530

    Initial Commit

---
+++ Tableview B to A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tableview B to A
+//
+//  Created by SAISURAJ on 1/9/20.
+//  Copyright © 2020 sai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
